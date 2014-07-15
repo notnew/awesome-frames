2e32e2ef381499479402d4101b575f1759a7d8e0
notnew <you@example.com>
2014-07-15 11:13:26 -0500

    simple emacs-frame like functionality
    frames are like emacs windows
    currently just has 3 fixed frames, and displays the first 3 clients in
    the frames, no switching frames, pulling clients into frames, adding or
    resizing frames, etc.

---
+++ frame.lua
@@ -0,0 +1,102 @@
+local ipairs = ipairs
+local awful = require("awful")
+local tag = require("awful.tag")
+local client = client
+
+local frame = {}
+
+frame.name = "frame"
+
+-- frames are tables containing
+-- geometry, a geometry table that can by passed to client.geometry (x,y,widht,
+-- height)
+-- client, a focused client for the frame
+local uzbl_frame = {
+    geometry =
+        function (p)
+            local wa = p.workarea
+            local t = tag.selected(p.screen)
+            local mwidth = wa.width * tag.getmwfact(t)
+
+            return { width = mwidth, height = wa.height, x = wa.x, y = wa.y }
+        end,
+    client = nil
+}
+
+local emacs_frame = {
+    geometry =
+        function (p)
+            local wa = p.workarea
+            local t = tag.selected(p.screen)
+            local mwidth = wa.width * tag.getmwfact(t)
+
+            return { width = wa.width - mwidth, height = wa.height/2,
+                     x = mwidth, y = wa.y }
+        end,
+    client = nil
+}
+
+local urxvt_frame = {
+    geometry =
+        function (p)
+            local wa = p.workarea
+            local t = tag.selected(p.screen)
+            local mwidth = wa.width * tag.getmwfact(t)
+
+            return { width = wa.width - mwidth, height = wa.height - wa.height/2,
+                     x = mwidth, y = wa.y + wa.height/2 }
+        end,
+    client = nil
+}
+
+local frames = {uzbl_frame, emacs_frame, urxvt_frame
+               , focus = 1 }
+
+local function client_focus()
+    local focused_frame = frames[frames.focus]
+    local new_focus = focused_frame and focused_frame.client
+    if new_focus then client.focus = new_focus end
+end
+
+function frame.focus_next_frame(rel_idx)
+    local count = #frames
+    if count == 0 then
+        local pass = true
+    elseif frames.focus < count and frames.focus > 0 then
+        frames.focus = frames.focus + 1
+    else
+        frames.focus = 1
+    end
+
+    client_focus()
+end
+
+local function add_client(client)
+    for _,frame in ipairs(frames) do
+        if frame.client == nil then
+            frame.client = client
+            return
+        end
+    end
+end
+
+function frame.arrange(p)
+    local cls = p.clients
+
+    for _,client in ipairs(cls) do
+        add_client(client)
+    end
+
+    for _,frame in ipairs(frames) do
+        local client = frame.client
+        if client then
+            client:geometry(frame.geometry(p))
+            client:raise()
+        end
+    end
+
+    client_focus()
+end
+
+return frame
+
